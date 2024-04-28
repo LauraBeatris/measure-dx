cfab08dd90548c07ebe831987a1a95b1b4b0fa04
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-04-28 13:24:31 -0300

    chore(api): Update leaderboard fixture

--- api/models/leaderboard.go
+++ api/models/leaderboard.go
@@ -21,21 +21,21 @@ var leaderboard = []*Tool{
 		Name:        "Vercel",
 		WebsiteUrl:  "https://vercel.com/",
 		AverageRate: "5.0",
-		LogoUrl:     "https://avatars.githubusercontent.com/u/109384852?v=4",
+		LogoUrl:     "https://avatars.githubusercontent.com/u/14985020?s=200&v=4",
 	},
 	{
 		ID:          "3",
-		Name:        "Vercel",
-		WebsiteUrl:  "https://vercel.com/",
+		Name:        "Railway",
+		WebsiteUrl:  "https://www.railway.app/",
 		AverageRate: "5.0",
-		LogoUrl:     "https://avatars.githubusercontent.com/u/109384852?v=4",
+		LogoUrl:     "https://avatars.githubusercontent.com/u/66716858?s=200&v=4",
 	},
 	{
 		ID:          "4",
-		Name:        "Vercel",
-		WebsiteUrl:  "https://vercel.com/",
+		Name:        "Basehub",
+		WebsiteUrl:  "https://basehub.com",
 		AverageRate: "5.0",
-		LogoUrl:     "https://avatars.githubusercontent.com/u/109384852?v=4",
+		LogoUrl:     "https://avatars.githubusercontent.com/u/126886981?s=200&v=4",
 	},
 }
 
