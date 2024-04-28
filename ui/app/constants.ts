e0c72b4ea80f0dadf0953b530bbf24a15fb3e826
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-04-28 13:47:53 -0300

    feat(ui): Implement footer

---
+++ ui/app/constants.ts
@@ -0,0 +1,5 @@
+export const links = {
+  githubUrl: 'https://github.com/LauraBeatris',
+  bookUrl: 'https://dx.addy.ie/',
+  vercelUrl: 'https://www.vercel.com/?utm_source=measure-dx&utm_campaign=oss',
+};
