a182901af177a672e17ad536703dce9977423fbc
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-05-01 19:49:37 -0300

    chore(ui): Extract primary header to separate component

--- ui/app/get-started/page.tsx
+++ ui/app/get-started/page.tsx
@@ -1,6 +1,7 @@
 import { Heading, HeadingLevel } from '@ariakit/react';
 import { SelectTool } from '../components/SelectTool';
 import { listTools } from '../lib/api';
+import { PrimaryHeader } from '../components/layout/PrimaryHeader';
 
 export default async function GetStartedPage() {
   const tools = await listTools();
@@ -17,23 +18,7 @@ export default async function GetStartedPage() {
           </span>
         </div>
         <HeadingLevel>
-          <div className="mb-4 flex items-center justify-center gap-1 md:gap-4">
-            <div
-              className="animate-fade-up opacity-0"
-              style={{ animationDelay: '0.15s', animationFillMode: 'forwards' }}
-            >
-              <div className="relative flex h-10 w-10 animate-pulse items-center justify-center rounded-lg border-2 border-gray-100 bg-gradient-to-r from-emerald-500 to-emerald-400 md:h-20 md:w-20">
-                <span className="text-2xl md:text-6xl">📏</span>
-              </div>
-            </div>
-
-            <Heading
-              className="animate-fade-up font-display bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem] dark:from-gray-300 dark:to-gray-50"
-              style={{ animationDelay: '0.15s', animationFillMode: 'forwards' }}
-            >
-              Measure DX
-            </Heading>
-          </div>
+          <PrimaryHeader />
         </HeadingLevel>
       </header>
 
--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { Leaderboard } from './components/Leaderboard';
 import { GitHubIcon } from './components/icons/GitHubIcon';
 import { links } from './constants';
+import { PrimaryHeader } from './components/layout/PrimaryHeader';
 
 // TODO - Make it responsive on mobile
 export default function Home() {
@@ -10,23 +11,7 @@ export default function Home() {
     <main className="flex h-full min-h-screen w-full flex-col items-center justify-center gap-5 p-5 md:p-0">
       <header className="flex flex-col text-center">
         <HeadingLevel>
-          <div className="mb-4 flex items-center justify-center gap-1 md:gap-4">
-            <div
-              className="animate-fade-up opacity-0"
-              style={{ animationDelay: '0.15s', animationFillMode: 'forwards' }}
-            >
-              <div className="relative flex h-10 w-10 animate-pulse items-center justify-center rounded-lg border-2 border-gray-100 bg-gradient-to-r from-emerald-500 to-emerald-400 md:h-20 md:w-20">
-                <span className="text-2xl md:text-6xl">📏</span>
-              </div>
-            </div>
-
-            <Heading
-              className="animate-fade-up font-display bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem] dark:from-gray-300 dark:to-gray-50"
-              style={{ animationDelay: '0.15s', animationFillMode: 'forwards' }}
-            >
-              Measure DX
-            </Heading>
-          </div>
+          <PrimaryHeader />
           <HeadingLevel>
             <Heading
               className="animate-fade-up mb-2 text-lg text-gray-800 opacity-0 md:text-xl dark:text-gray-100"
