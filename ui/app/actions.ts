e85a8bdc10eeb8705173ac4ebadb6d9d1c0b9e82
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-05-01 19:11:28 -0300

    chore(ui): Structure rate tool form

---
+++ ui/app/actions.ts
@@ -0,0 +1,4 @@
+// TODO - Implement tool rating
+export async function rateTool() {
+  console.log('Hello World');
+}
--- ui/app/components/RateToolForm.tsx
+++ ui/app/components/RateToolForm.tsx
@@ -2,56 +2,21 @@
 
 import * as Ariakit from '@ariakit/react';
 import Link from 'next/link';
-import { useId } from 'react';
+import { useState } from 'react';
 import { RateArea } from '../lib/api';
+import { rateTool } from '../actions';
 
 interface RateToolFormProps {
   rateAreas: RateArea[];
 }
 
 export function RateToolForm({ rateAreas }: RateToolFormProps) {
-  const id = useId();
-  const form = Ariakit.useFormStore({ defaultValues: { color: '' } });
-
   return (
-    <Ariakit.Form
-      store={form}
-      aria-labelledby={id}
-      className="mt-2 flex flex-col"
-    >
+    <form action={rateTool} className="mt-2 flex flex-col">
       {rateAreas.map(({ id, title }) => (
-        <Ariakit.FormRadioGroup key={id} className="mb-2 flex w-full gap-2">
-          <Ariakit.FormGroupLabel className="mb-3 grow pr-2.5 font-bold text-gray-900 sm:mb-0 sm:w-72 sm:font-normal">
-            {title}
-          </Ariakit.FormGroupLabel>
-
-          <div className="grid grid-cols-3 gap-x-1.5 tracking-tight sm:tracking-normal">
-            <label className="rate-option justify-center">
-              <Ariakit.FormRadio
-                className="top-50 left-50 absolute cursor-pointer opacity-0"
-                name="bad"
-                value={0}
-              />
-              Bad
-            </label>
-            <label className="rate-option justify-center">
-              <Ariakit.FormRadio
-                className="top-50 left-50 absolute cursor-pointer opacity-0"
-                name="so-so"
-                value={5}
-              />
-              So-so
-            </label>
-            <label className="rate-option justify-center">
-              <Ariakit.FormRadio
-                className="top-50 left-50 absolute cursor-pointer opacity-0"
-                name="good"
-                value={10}
-              />
-              Good
-            </label>
-          </div>
-        </Ariakit.FormRadioGroup>
+        <Ariakit.RadioProvider key={id}>
+          <RateAreaRadioGroup title={title} />
+        </Ariakit.RadioProvider>
       ))}
 
       <footer className="ml-auto flex w-full items-end justify-end">
@@ -61,10 +26,76 @@ export function RateToolForm({ rateAreas }: RateToolFormProps) {
         >
           <span>Back</span>
         </Link>
-        <Ariakit.FormSubmit className="focus-visible:ariakit-outline mt-1 flex h-12 w-max items-center justify-center gap-1 whitespace-nowrap rounded-lg bg-emerald-500 px-4 font-medium text-gray-50 shadow-xl hover:bg-emerald-600 sm:px-8 sm:text-lg">
+        <button className="focus-visible:ariakit-outline mt-1 flex h-12 w-max items-center justify-center gap-1 whitespace-nowrap rounded-lg bg-emerald-500 px-4 font-medium text-gray-50 shadow-xl hover:bg-emerald-600 sm:px-8 sm:text-lg">
           Submit
-        </Ariakit.FormSubmit>
+        </button>
       </footer>
-    </Ariakit.Form>
+    </form>
+  );
+}
+
+interface RateAreaProps extends Omit<RateArea, 'id'> {}
+
+function RateAreaRadioGroup({ title }: RateAreaProps) {
+  const [currentChecked, setCurrentChecked] = useState(0);
+
+  return (
+    <Ariakit.RadioGroup
+      onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+        setCurrentChecked(Number(event.target.value))
+      }
+      className="mb-2 flex w-full gap-2"
+    >
+      <label className="mb-3 grow pr-2.5 font-bold text-gray-900 sm:mb-0 sm:w-72 sm:font-normal">
+        {title}
+      </label>
+
+      <div className="grid grid-cols-3 gap-x-1.5 tracking-tight sm:tracking-normal">
+        <RateOption
+          label="Bad"
+          name="bad"
+          value={0}
+          currentChecked={currentChecked}
+        />
+
+        <RateOption
+          label="So-so"
+          name="average"
+          value={5}
+          currentChecked={currentChecked}
+        />
+
+        <RateOption
+          label="Good"
+          name="good"
+          value={10}
+          currentChecked={currentChecked}
+        />
+      </div>
+    </Ariakit.RadioGroup>
+  );
+}
+
+interface RateOptionProps {
+  currentChecked: number;
+  value: number;
+  name: string;
+  label: string;
+}
+
+function RateOption({ label, currentChecked, value, name }: RateOptionProps) {
+  const isChecked = currentChecked === value;
+
+  return (
+    <label
+      className={`rate-option justify-center ${isChecked ? '!bg-gray-200' : ''}`}
+    >
+      <Ariakit.Radio
+        className="top-50 left-50 absolute cursor-pointer opacity-0"
+        name={name}
+        value={value}
+      />
+      {label}
+    </label>
   );
 }
