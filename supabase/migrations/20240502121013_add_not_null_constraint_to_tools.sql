b2b7b1fe711d6dc0f052945561112c5b856c5ab8
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-05-02 09:22:04 -0300

    chore(supabase): Add `NOT NULL` constraint to `tools` columns

---
+++ supabase/migrations/20240502121013_add_not_null_constraint_to_tools.sql
@@ -0,0 +1,34 @@
+ALTER TABLE
+  tools
+ALTER COLUMN
+  name
+SET
+  NOT NULL;
+
+ALTER TABLE
+  tools
+ALTER COLUMN
+  logo_url
+SET
+  NOT NULL;
+
+ALTER TABLE
+  tools
+ALTER COLUMN
+  website_url
+SET
+  NOT NULL;
+
+ALTER TABLE
+  tools
+ALTER COLUMN
+  average_rate
+SET
+  NOT NULL;
+
+ALTER TABLE
+  tools
+ALTER COLUMN
+  created_at
+SET
+  NOT NULL;
