4dec49261dbbdfc77f40e4becaf71408e1c3124a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-05-13 09:27:46 -0300

    feat: Introduce combobox for tools

--- ui/app/components/SelectTool.tsx
+++ ui/app/components/SelectTool.tsx
@@ -7,9 +7,10 @@ import { startTransition, useMemo, useState } from 'react';
 import { PaperPlanIcon } from './icons/PaperPlanIcon';
 import { SpinnerIcon } from './icons/SpinnerIcon';
 import { Tables } from '../types/supabase';
-import { className } from '../lib/className';
 import { createClient } from '../lib/supabase/client';
 import { getBaseUrl } from '../lib/baseUrl';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
 type Tool = Tables<'tools'>;
 
@@ -20,7 +21,7 @@ interface SelectToolProps {
 export function SelectTool({ tools }: SelectToolProps) {
   const [isNavigating, setIsNavigating] = useState(false);
   const [searchValue, setSearchValue] = useState('');
-  const [selectedTool, setSelectedTool] = useState<Tool | undefined>(tools[0]);
+  const [selectedValue, setSelectedValue] = useState<string>(tools[0].name);
 
   const matches = useMemo(() => {
     return matchSorter(tools, searchValue, {
@@ -30,11 +31,15 @@ export function SelectTool({ tools }: SelectToolProps) {
   }, [searchValue, tools]);
 
   async function handleClick() {
-    if (!selectedTool) return;
-
     setIsNavigating(true);
 
-    const next = `/tools/${selectedTool.id}`;
+    const tool = tools.find((tool) => tool.name === selectedValue);
+
+    if (!tool) {
+      return notFound();
+    }
+
+    const next = `/tools/${tool.id}`;
     const redirectTo = `${getBaseUrl()}/auth/callback?next=${next}`;
 
     const supabase = createClient();
@@ -46,71 +51,61 @@ export function SelectTool({ tools }: SelectToolProps) {
     });
   }
 
-  const isDisabled = !selectedTool;
-
   return (
     <div className="wrapper">
-      <Ariakit.ComboboxProvider
-        resetValueOnHide
+      <Ariakit.ComboboxProvider<string>
+        setSelectedValue={(value) => setSelectedValue(value)}
         setValue={(value) => {
-          startTransition(() => {
-            setSearchValue(value);
-          });
+          startTransition(() => setSearchValue(value));
         }}
+        defaultSelectedValue={tools[0].name}
+        defaultValue={tools[0].name}
       >
-        <Ariakit.SelectProvider
-          setValue={(value: string) => {
-            startTransition(() => {
-              const tool = tools.find((tool) => tool.name === value);
-              setSelectedTool(tool);
-            });
-          }}
+        <Ariakit.ComboboxLabel
+          className="label text-center text-lg text-gray-800 md:text-xl dark:text-gray-100"
+          render={<h2 />}
         >
-          <Ariakit.SelectLabel
-            className="label mb-2 text-center text-lg text-gray-800  md:text-xl dark:text-gray-100"
-            render={<h2 />}
-          >
-            Choose a tool to measure:
-          </Ariakit.SelectLabel>
-          <Ariakit.Select autoFocus className="button" />
-          <Ariakit.SelectPopover gutter={4} sameWidth className="popover">
-            <div className="combobox-wrapper">
-              <Ariakit.Combobox
-                autoSelect
-                placeholder="Search..."
-                className="combobox"
-              />
-            </div>
-            <Ariakit.ComboboxList>
-              {matches.map(({ id, name }) => (
-                <Ariakit.SelectItem
-                  key={id}
-                  value={name}
-                  className="select-item"
-                  render={<Ariakit.ComboboxItem />}
+          Choose a tool to measure:
+        </Ariakit.ComboboxLabel>
+        <Ariakit.Combobox className="combobox" autoFocus />
+        <Ariakit.ComboboxPopover gutter={4} sameWidth className="popover">
+          {matches.length ? (
+            matches.map((tool) => (
+              <Ariakit.ComboboxItem
+                key={tool.id}
+                className="combobox-item"
+                value={String(tool.name)}
+              >
+                <Image
+                  src={tool.logo_url}
+                  width={5}
+                  height={5}
+                  alt={tool.name}
+                  quality={100}
+                  className="pointer-events-none h-5 w-5 rounded-md blur-0"
                 />
-              ))}
-            </Ariakit.ComboboxList>
-          </Ariakit.SelectPopover>
-        </Ariakit.SelectProvider>
+
+                {tool.name}
+              </Ariakit.ComboboxItem>
+            ))
+          ) : (
+            <div className="no-results">No results found</div>
+          )}
+        </Ariakit.ComboboxPopover>
       </Ariakit.ComboboxProvider>
 
       <Ariakit.Button
-        disabled={!selectedTool}
         onClick={handleClick}
-        className={className(
-          'focus-visible:ariakit-outline mt-2 flex h-12 items-center justify-center gap-1 whitespace-nowrap rounded-lg bg-emerald-500 px-4 font-medium text-gray-50 shadow-xl hover:bg-emerald-600 sm:px-8 sm:text-lg',
-          {
-            'cursor-not-allowed opacity-50': isDisabled,
-          },
-        )}
+        className={
+          'focus-visible:ariakit-outline mt-2 flex h-12 items-center justify-center gap-1 whitespace-nowrap rounded-lg bg-emerald-500 px-4 font-medium text-gray-50 shadow-xl hover:bg-emerald-600 sm:px-8 sm:text-lg'
+        }
       >
         {isNavigating ? (
           <SpinnerIcon />
         ) : (
           <>
             {' '}
-            <span>Get started</span>
+            <span>Measure</span>
             <PaperPlanIcon />
           </>
         )}
