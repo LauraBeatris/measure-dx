b469c33ee6042fc336af12dd46410c1674a4931f
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-07-08 08:55:16 -0300

    Restructure share page

--- ui/app/share/[id]/page.tsx
+++ ui/app/share/[id]/page.tsx
@@ -1,7 +1,9 @@
+import { PaperPlanIcon } from '@/app/components/icons/PaperPlanIcon';
 import { PrimaryHeader } from '@/app/components/layout/PrimaryHeader';
 import { createClient } from '@/app/lib/supabase/server';
-import { Button, HeadingLevel } from '@ariakit/react';
+import { HeadingLevel } from '@ariakit/react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 interface SharePageProps {
@@ -10,7 +12,6 @@ interface SharePageProps {
   };
 }
 
-// TODO - Add sharing + button to go back and measure more tools
 export default async function SharePage({ params }: SharePageProps) {
   const client = await createClient<any>();
 
@@ -40,28 +41,44 @@ export default async function SharePage({ params }: SharePageProps) {
       .single()
   ).data;
 
-  console.log({ tool });
-
   return (
     <main className="flex h-full min-h-screen w-full flex-col items-center justify-center">
-      <HeadingLevel>
-        <PrimaryHeader />
-      </HeadingLevel>
-
+      <header className="flex flex-col text-center">
+        <div
+          className="text-3xs mx-auto mb-1 w-max animate-fade-up rounded border px-1.5 py-px font-medium text-gray-500 opacity-0"
+          style={{ animationDelay: '0.15s', animationFillMode: 'forwards' }}
+        >
+          <span className="text-md flex items-center gap-1 p-1 text-gray-500 dark:text-gray-100">
+            You've measured{' '}
+            <strong className="font-medium text-gray-700">{tool.name}</strong>
+            <Image
+              src={tool.logo_url}
+              alt={`${tool.name}'s logo`}
+              width={16}
+              height={16}
+              className="rounded-md"
+            />
+          </span>
+        </div>
+        <HeadingLevel>
+          <PrimaryHeader />
+        </HeadingLevel>
+      </header>
       <div
-        className="flex animate-fade-up items-center gap-1 opacity-0"
+        className="flex animate-fade-up items-center justify-center gap-1 opacity-0"
         style={{ animationDelay: '0.25s', animationFillMode: 'forwards' }}
       >
-        <h2 className="text-xl">Thanks for measuring {tool.name}</h2>
-
-        <Image
-          src={tool.logo_url}
-          alt={`${tool.name}'s logo`}
-          width={24}
-          height={12}
-          className="rounded-md"
-        />
+        <h2 className="w-9/12 text-center text-xl text-gray-900">
+          Your feedback helps the overall community in the effort of seeking the
+          best experience for our favorite tools
+        </h2>
       </div>
+      <Link
+        href="/get-started"
+        className="focus-visible:ariakit-outline mt-4 flex h-12 w-max items-center justify-center gap-1 whitespace-nowrap rounded-lg bg-emerald-500 px-4 font-medium text-gray-50 shadow-xl hover:bg-emerald-600 sm:px-8 sm:text-lg"
+      >
+        Continue with other tools <PaperPlanIcon />
+      </Link>
     </main>
   );
 }
